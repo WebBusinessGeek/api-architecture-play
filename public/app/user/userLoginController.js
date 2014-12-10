fe6557712a7d9979df73b828791eb45e9bfb4456
Kevin Washington <kevw12188@gmail.com>
2014-12-10 17:21:33 -0500

    more set up

--- public/app/user/userLoginController.js
+++ public/app/user/userLoginController.js
@@ -2,8 +2,17 @@
  * Created by MacBookEr on 12/10/14.
  */
 angular.module('app')
-    .controller('userLoginController', ['$scope', function($scope){
+    .controller('userLoginController', ['$scope','userLoginService', function($scope, userLoginService){
 
         $scope.stuff = 'login';
 
-    }]);
+        $scope.stuff2 = userLoginService.service;
+    }])
+
+
+    .service('userLoginService', function(){
+
+        this.service = 'userLoginService';
+    });
+
+
